4be0a7e781b01b7a6aa62e93faa2fae02f89378a
Anand VM <anandnambissan2@gmail.com>
2023-09-09 15:11:57 +0530

    updated code

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def greet():
+    return jsonify(message="Hello from Flask-Wsgi App!")
+
+@app.route('/api/items')
+def items():
+    sample_items = [{"id": 1, "name": "item1"}, {"id": 2, "name": "item2"}]
+    return jsonify(items=sample_items)
+
+if __name__ == "__main__":
+    app.run(debug=True)
