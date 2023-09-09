74f84a54e66117277d82368a3001dfb930fbb85b
Anand VM <anandnambissan2@gmail.com>
2023-09-09 15:28:55 +0530

    updated codes 2

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, jsonify
+from api import auth
 
 app = Flask(__name__)
 
-@app.route('/')
-def greet():
-    return jsonify(message="Hello from Flask-Wsgi App!")
 
-@app.route('/api/items')
-def items():
-    sample_items = [{"id": 1, "name": "item1"}, {"id": 2, "name": "item2"}]
-    return jsonify(items=sample_items)
+app.register_blueprint(auth.bp)
+
+
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
